b430395098e96097678f52fb1593584fb0358018
gabimartini <gabimartinii@hotmail.com>
2023-11-24 16:22:53 +0000

    add header

---
+++ src/components/header/index.test.js
@@ -0,0 +1,8 @@
+import Header from ".";
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+test("it should render correctly", () => {
+    render(<Header/>)
+    expect(screen.getByRole('definition', {definition: "Planning applications"}))
+})
