686ecda1338a979141a099f07ccd0fc0f48c5f86
gabimartini <gabimartinii@hotmail.com>
2023-11-24 16:22:38 +0000

    add details

---
+++ src/components/details/index.test.js
@@ -0,0 +1,8 @@
+import Details from ".";
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+test("it should render correctly", () => {
+    render(<Details summary="How to write good feedback"/>)
+    expect(screen.getByRole('definition', {definition: "How to write good feedback"}))
+})
