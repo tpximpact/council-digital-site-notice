cb77c9a704b17c7469be27d71ad3367cf08b9970
gabimartini <gabimartinii@hotmail.com>
2023-11-24 16:23:09 +0000

    add input

---
+++ src/components/input/index.test.js
@@ -0,0 +1,9 @@
+import Input from ".";
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+test("it should render correctly", () => {
+    render(<Input/>)
+    expect(screen.getByRole('textbox', {type: 'text'}))
+    expect(screen.getByRole('definition', {definition: 'Enter a postcode to find planning applications nearby'}))
+})
---
+++ src/components/input/index.tsx
@@ -0,0 +1,14 @@
+const Input = ({label}:{label: string}) => {
+    return (
+        <div className="govuk-form-group">
+        <h1 className="govuk-label-wrapper">
+          <label className="govuk-label govuk-label--l" htmlFor="event-name" role="definition">
+              {label}
+          </label>
+        </h1>
+        <input className="govuk-input" id="event-name" name="eventName" type="text" role="textbox"/>
+      </div>
+    )
+}
+
+export default Input
