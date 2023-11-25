315736c2b6d58debdc223cd6b9d97841a7d93042
duncan <duncanuk@gmail.com>
2023-11-25 07:37:01 +0000

    uses env vars

--- util/client.ts
+++ util/client.ts
@@ -1,8 +1,8 @@
 import {createClient} from '@sanity/client'
 
 export const  client = createClient({
-    projectId: 'xx',
-    dataset: 'production',
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     useCdn: false, // set to `false` to bypass the edge cache
     apiVersion: '2023-11-15', // use current date (YYYY-MM-DD) to target the latest API version
     token: process.env.NEXT_PUBLIC_SANITY_SECRET_TOKEN // Only if you want to update content with the client
