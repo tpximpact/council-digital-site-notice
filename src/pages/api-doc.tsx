59438f2bdc68fbeefc48dd97cda0408a3a73bef1
Geraint <geraint87@gmail.com>
2023-11-29 09:36:18 +0000

    Fix swaggerui import

--- src/pages/api-doc.tsx
+++ src/pages/api-doc.tsx
@@ -2,10 +2,7 @@ import { GetStaticProps, InferGetStaticPropsType } from 'next';
 import { createSwaggerSpec } from 'next-swagger-doc';
 import dynamic from 'next/dynamic';
 import 'swagger-ui-react/swagger-ui.css';
-
-const SwaggerUI = dynamic<{
-  spec: any;
-}>(import('swagger-ui-react'), { ssr: false });
+import SwaggerUI from "swagger-ui-react";
 
 function ApiDoc({ spec }: InferGetStaticPropsType<typeof getStaticProps>) {
   return <SwaggerUI spec={spec} />;
