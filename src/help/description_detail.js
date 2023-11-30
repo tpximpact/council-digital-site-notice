f1ddb90cc1df0abae5b27f20315c9d8e8fcf37cc
gabimartini <gabimartinii@hotmail.com>
2023-11-30 11:06:33 +0000

    add feedback and feeling components

---
+++ src/help/description_detail.js
@@ -0,0 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
+export const descriptionDetail = {
+    "feeling": <>
+<p className="govuk-body-s">We ask for feedback on developments for a few different reasons. Understanding why we want your input can help you write better feedback and influence a proposed development in your area.</p>
+
+<p className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-0">To influence the details</p>
+<p className="govuk-body-s">Residents' knowledge about local needs can help planners decide things like provision of public open space, cycling facilities, land use priorities, scale, design and or what materials are appropriate for a scheme.</p>
+
+<p className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-0">To be our eyes and ears on the ground</p>
+<p className="govuk-body-s">Residents have in-depth knowledge about their local area and often highlight on-the-ground details - like how a public view could be affected or that a mature tree was left out of a developer's plans.</p>
+
+<p className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-0">To make sure we balance our priorities</p>
+<p className="govuk-body-s">A single development is unlikely to fulfil all the priorities for the local area. It's useful to be able to demonstrate which priorities are important to residents, so they can push developers to be more ambitious with their targets. Telling us what your priorities are can help planners make that decision.</p>
+    </>
+}
