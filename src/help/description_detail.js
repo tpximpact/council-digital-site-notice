c00ecd996a28466b952d6b500ca124222c9cc904
gabimartini <gabimartinii@hotmail.com>
2023-11-30 13:51:04 +0000

    impact component

--- src/help/description_detail.js
+++ src/help/description_detail.js
@@ -1,15 +1,35 @@
 /* eslint-disable react/no-unescaped-entities */
 export const descriptionDetail = {
     "feeling": <>
-<p className="govuk-body-s">We ask for feedback on developments for a few different reasons. Understanding why we want your input can help you write better feedback and influence a proposed development in your area.</p>
+        <p className="govuk-body-s">We ask for feedback on developments for a few different reasons. Understanding why we want your input can help you write better feedback and influence a proposed development in your area.</p>
 
-<p className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-0">To influence the details</p>
-<p className="govuk-body-s">Residents' knowledge about local needs can help planners decide things like provision of public open space, cycling facilities, land use priorities, scale, design and or what materials are appropriate for a scheme.</p>
+        <p className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-0">To influence the details</p>
+        <p className="govuk-body-s">Residents' knowledge about local needs can help planners decide things like provision of public open space, cycling facilities, land use priorities, scale, design and or what materials are appropriate for a scheme.</p>
 
-<p className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-0">To be our eyes and ears on the ground</p>
-<p className="govuk-body-s">Residents have in-depth knowledge about their local area and often highlight on-the-ground details - like how a public view could be affected or that a mature tree was left out of a developer's plans.</p>
+        <p className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-0">To be our eyes and ears on the ground</p>
+        <p className="govuk-body-s">Residents have in-depth knowledge about their local area and often highlight on-the-ground details - like how a public view could be affected or that a mature tree was left out of a developer's plans.</p>
 
-<p className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-0">To make sure we balance our priorities</p>
-<p className="govuk-body-s">A single development is unlikely to fulfil all the priorities for the local area. It's useful to be able to demonstrate which priorities are important to residents, so they can push developers to be more ambitious with their targets. Telling us what your priorities are can help planners make that decision.</p>
-    </>
+        <p className="govuk-body-s govuk-!-font-weight-bold govuk-!-margin-0">To make sure we balance our priorities</p>
+        <p className="govuk-body-s">A single development is unlikely to fulfil all the priorities for the local area. It's useful to be able to demonstrate which priorities are important to residents, so they can push developers to be more ambitious with their targets. Telling us what your priorities are can help planners make that decision.</p>
+            </>,
+    "impact": <>
+        <p className="govuk-body-s" >As part of the negotiation, the case officer can take into consideration all comments which are ‘material considerations’ to the proposed development. These include (but aren't limited to):</p>
+        <ul className="govuk-body-s">
+            <li>overlooking/loss of privacy</li>
+            <li>loss of light or overshadowing</li>
+            <li>traffic parking</li>
+            <li>highway safety</li>
+            <li>noise from new uses or plant equipment</li>
+            <li>effect on listed building and conservation area</li>
+            <li>scale of buildings and structures</li>
+            <li>layout and density of building</li>
+            <li>design, appearance and materials</li>
+            <li>disabled persons' access</li>
+            <li>previous planning decisions (including appeal decisions)</li>
+            <li>trees and nature conservation</li>
+        </ul>
+        <p className="govuk-body-s">Issues such as loss of private view or negative impact on property values, or civil matters like ‘right to light’, party walls and property damage are not considered ‘material planning considerations’.</p>
+        <p className="govuk-body-s">The case officer will summarise their findings in the officer's report and/or decision notice.</p>
+        <p className="govuk-body-s">We won't acknowledge receipt of your comments, or get in touch with you directly about the issues you've raised. You can check the officer's report or decision notice to see if your, and other, comments have been logged.</p>
+</>
 }
