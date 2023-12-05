28961f9340839e4558057dde863ef99c8ba04802
duncan <duncanuk@gmail.com>
2023-12-05 14:17:43 +0000

    updates

--- sanity/schemas/comment.ts
+++ sanity/schemas/comment.ts
@@ -3,14 +3,48 @@ import { defineField, defineType } from "sanity";
 export default defineType({
     title: "Comments",
     name: "comment",
-    type: "document",
+    type: "object",
     fields: [
+        defineField({
+            title: "name",
+            name: "name",
+            type: "string",
+            // readOnly: true,
+          }),
+          defineField({
+            title: "address",
+            name: "address",
+            type: "string",
+            // readOnly: true,
+          }),
+          defineField({
+            title: "postcode",
+            name: "postcode",
+            type: "string",
+            // readOnly: true,
+            hidden: true,
+          }),
+          defineField({
+            title: "telephone",
+            name: "telephone",
+            type: "string",
+            // readOnly: true,
+          }),
         defineField({
           title: "email",
           name: "email",
           type: "string",
           validation: (Rule: any) => Rule.email(),
-          readOnly: true,
+        //   readOnly: true,
         }),
-    ]
+        defineField({
+          title: "comment",
+          name: "comment",
+          type: "text",
+        //   validation: (Rule: any) => Rule.required(),
+        //   readOnly: true,
+        }),
+        
+    ],
+    
 });
