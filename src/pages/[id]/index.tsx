34646bd6841e0c658dc757ddce376fa2a00b57ab
gabimartini <gabimartinii@hotmail.com>
2023-12-06 16:48:02 +0000

    unhide props

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -4,49 +4,49 @@ import Impact from "./impact";
 import Process from "./process";
 import { ContextApplication } from "@/context";
 import { useContext, useEffect } from "react";
-// import { getActiveApplications, getActiveApplicationById } from "../../../util/client";
+import { getActiveApplications, getActiveApplicationById } from "../../../util/client";
 
-// export async function getStaticProps(context: any) {
-//   const {id} = context.params;
-//   const data = await getActiveApplicationById(id)
-//   return {
-//     props: {
-//       data: data[0]
-//     },
-//   };
-// }
+export async function getStaticProps(context: any) {
+  const {id} = context.params;
+  const data = await getActiveApplicationById(id)
+  return {
+    props: {
+      data: data[0]
+    },
+  };
+}
 
-// export async function getStaticPaths() {
-//   const data = await getActiveApplications();
-//   return {
-//   paths: data.map((doc: any) => ({params: {data: doc, id: doc._id}})),
-//   fallback: false,
-//   }
-// }
+export async function getStaticPaths() {
+  const data = await getActiveApplications();
+  return {
+  paths: data.map((doc: any) => ({params: {data: doc, id: doc._id}})),
+  fallback: false,
+  }
+}
 
-const Application = () => {
+const Application = ({data}: {data:any}) => {
   const {setDataApplication} = useContext(ContextApplication)
-  // const {name} = data
+  const {name} = data
 
-  // useEffect(() => {
-  //   setDataApplication(data)
-  //   localStorage.setItem("application", JSON.stringify({
-  //     'address': data.address,
-  //     'image': data.image,
-  //     'deadline': data.commentDeadline
-  //   }))
-  // },[])
+  useEffect(() => {
+    setDataApplication(data)
+    localStorage.setItem("application", JSON.stringify({
+      'address': data.address,
+      'image': data.image,
+      'deadline': data.commentDeadline
+    }))
+  },[])
   
 
-const breadcrumbs_array = [{name: "Planning applications", href: "/"}, {name: "", href:""}]
+const breadcrumbs_array = [{name: "Planning applications", href: "/"}, {name: name, href:""}]
 
 
     return (
         <>
         <Breadcrumbs breadcrumbs_info={breadcrumbs_array}/>
-        <About data={[]}/>
+        <About data={data}/>
         <Impact />
-        <Process data={[]}/>
+        <Process data={data}/>
         </>
     )
 }
