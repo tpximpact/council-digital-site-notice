c4b46cb87104f9551bd5dc0e380befcda67c9a2c
gabimartini <gabimartinii@hotmail.com>
2023-12-07 16:00:00 +0000

    persist details data

--- src/pages/[id]/feedback/questions/personal-details.tsx
+++ src/pages/[id]/feedback/questions/personal-details.tsx
@@ -2,6 +2,7 @@ import {Button, BackLink} from "@/components/button"
 import Details from "@/components/details";
 import Input from "@/components/input";
 import { descriptionDetail } from "../../../../../util/description_detail";
+import { useEffect, useState } from "react";
 
 function PersonalDetails({
         onChange, 
@@ -10,8 +11,30 @@ function PersonalDetails({
         setQuestion,
         selectedCheckbox
     }: {onChange: () => void, personalDetailsForm: any, setPersonalDetailsForm: (value: any) => void, setQuestion:(value:any) => void, selectedCheckbox: any}) {
+        const [defaultValue, setDefaultValue] = useState({name: '', email: '', phone: '', postcode: ''})
     
         const backComponent = selectedCheckbox[selectedCheckbox.length - 1]
+
+        useEffect(() => {
+            const initialValueName = localStorage.getItem("name") || ''
+            const initialValueEmail = localStorage.getItem("email") || ''
+            const initialValuePhone = localStorage.getItem("phone") || ''
+            const initialValuePostcode = localStorage.getItem("postcode") || ''
+
+            const {name, email, phone, postcode} = personalDetailsForm
+
+            setDefaultValue({
+                name: name || initialValueName,
+                email: email || initialValueEmail,
+                phone: phone || initialValuePhone,
+                postcode: postcode || initialValuePostcode
+            })
+        }, [personalDetailsForm])
+
+        const onChangeDetails = (value:any, key:any) => {
+            setPersonalDetailsForm({...personalDetailsForm, key: value})
+            localStorage.setItem(key, value)
+        }
 
     return(
         <section className="wrap-personal-details">
@@ -20,10 +43,10 @@ function PersonalDetails({
         <p className="govuk-body-s">If the your comment relates to an issue that will affect you personally (for example, the development will block light coming into your home or affect your privacy), we need some of your details.</p>
         <p className="govuk-body-s">This is because we can only formally explore comments coming from people who live close to the proposed development.</p>
         <Details summary="How we handle your data" description={descriptionDetail['personal-details']}/>
-        <Input label="Your name" hint="Optional" onChange={(value: any) => setPersonalDetailsForm({...personalDetailsForm, 'name': value})} value={personalDetailsForm?.name} type='text'/>
-        <Input label="Your email address" hint="Optional" onChange={(value: any) => setPersonalDetailsForm({...personalDetailsForm, 'email': value})} value={personalDetailsForm?.email} type='email'/>
-        <Input label="Your telephone number" hint="Optional" onChange={(value: any) => setPersonalDetailsForm({...personalDetailsForm, 'phone': value})} value={personalDetailsForm?.phone} type='tel'/>
-        <Input label="Your postcode" hint="Optional" onChange={(value: any) => setPersonalDetailsForm({...personalDetailsForm, 'postcode': value})} value={personalDetailsForm?.postcode} type='text'/>
+        <Input label="Your name" hint="Optional" onChange={(value: any) => onChangeDetails(value, 'name')} value={defaultValue?.name} type='text'/>
+        <Input label="Your email address" hint="Optional" onChange={(value: any) => onChangeDetails(value, 'email')} value={defaultValue?.email} type='email'/>
+        <Input label="Your telephone number" hint="Optional" onChange={(value: any) => onChangeDetails(value, 'phone')} value={defaultValue?.phone} type='tel'/>
+        <Input label="Your postcode" hint="Optional" onChange={(value: any) => onChangeDetails(value, 'postcode')} value={defaultValue?.postcode} type='text'/>
         <Button content="Submit your comments" onClick={() => {onChange()}}/>
         </section>
         )
