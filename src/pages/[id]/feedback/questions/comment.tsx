92aa22c3be3e29352c28f26c89cc0dbb66be00ee
gabimartini <gabimartinii@hotmail.com>
2023-12-07 15:59:39 +0000

    persistent comment data

--- src/pages/[id]/feedback/questions/comment.tsx
+++ src/pages/[id]/feedback/questions/comment.tsx
@@ -1,5 +1,6 @@
 import TextArea from "@/components/text-area"
 import {Button, BackLink} from "@/components/button"
+import { useEffect, useState } from "react"
 
 function CommentQuestion({
     onChange, 
@@ -9,19 +10,36 @@ function CommentQuestion({
     setQuestion, 
     selectedCheckbox,
     question}: 
-    {onChange: () => void, label: string, commentForm: any, setCommentForm: (value: string) => void, setQuestion: (value:number) => void, selectedCheckbox: any, question: number}) {
-    const indexComponent = selectedCheckbox.indexOf(question)
-    const backComponent = indexComponent > 0 ? selectedCheckbox[indexComponent - 1] : 2
+    {onChange: () => void, label: string, commentForm: any, setCommentForm: (value: any) => void, setQuestion: (value:number) => void, selectedCheckbox: any, question: number}) {
+    const [defaultValue, setDefaultValue] = useState('')
+        const indexComponent = selectedCheckbox.indexOf(question)
+        const backComponent = indexComponent > 0 ? selectedCheckbox[indexComponent - 1] : 2
 
-    const defaultValue = commentForm[question]
+    useEffect(() => {
+        const initialValue = localStorage.getItem("comment")
+        if ( Object.keys(commentForm).length > 0  || initialValue === null) {
+            setDefaultValue(commentForm[question] !== undefined ? commentForm[question] : "")
+        } else {
+
+        setDefaultValue(JSON.parse(initialValue)[question])
+        setCommentForm(JSON.parse(initialValue))
+        }
+    }, [commentForm, question, setDefaultValue, setCommentForm])
+
+    const onComment = (value:any) => {
+        setCommentForm({...commentForm,[question]: value})
+        localStorage.setItem('comment', JSON.stringify({...commentForm,[question]: value}))
+    }
+
     return(
         <section>
             <BackLink content='Back'onClick={() => setQuestion(backComponent)}/>
             <h1 className="govuk-heading-l">Comment on:</h1>
             <TextArea 
-                label={label} onChange={(value:any) => setCommentForm({...commentForm ,[question]: value})} 
+                label={label} 
+                onChange={(value:any) => onComment(value)} 
                 value={defaultValue}
-                key={question}/>
+                id={question}/>
             <Button content="Next" onClick={() => {onChange()}}/>
         </section>
     )
