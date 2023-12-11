4b048400934bee8125ab98222cf29556c4c4513b
gabimartini <gabimartinii@hotmail.com>
2023-12-11 09:29:49 +0000

    add react paginate

---
+++ src/pages/app/pagination.tsx
@@ -0,0 +1,41 @@
+import ReactPaginate from 'react-paginate';
+
+
+const Pagination = ({data, onSelectPage, itemsPerPage}: any) => {
+
+const pageCount = Math.ceil(data.length / itemsPerPage);
+
+const handlePageClick = (event: any) => {
+    const newOffset = (event.selected * itemsPerPage) % data.length;
+    const newId = data[newOffset]
+    onSelectPage(newId)
+  };
+
+return(
+    <div className="wrap-pagination">
+        <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={2}
+        marginPagesDisplayed={1}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination govuk-body"
+        activeClassName="active"
+        renderOnZeroPageCount={null}
+      />
+
+    </div>
+)
+}
+
+export default Pagination;
