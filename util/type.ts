a07f8b70b8c1a9845ca5a469f5e3aa3ed1e284dd
gabimartini <gabimartinii@hotmail.com>
2024-01-05 17:05:16 +0000

    update types

--- util/type.ts
+++ util/type.ts
@@ -1,22 +1,62 @@
-import { Id, Image } from "sanity"
+import { Id } from "sanity"
+
+export type Housing = {
+    affordableResidentialUnits: number
+    residentialUnits: number
+}
+
+export type Jobs = {
+    min: number
+    max: number
+}
+
+export type ProposedLandUse = {
+    classB: boolean
+    classC: boolean
+    classE: boolean
+    classF: boolean
+    suiGeneris: boolean
+    suiGenerisDetail: string
+}
 
 export type Data = {
     _id: Id
     name: string
-    address: string
-    description: string
-    height: number
-    reference: string
-    commentDeadline: string
-    applicationType: string
+    development_address: string
     image: string
 }
 
-export type DataObj = {
-    data: Data
+export type DataDetails = {
+    massings?: string
+    name?: string
+    development_description?: string
+    development_address?: string
+    application_type?: string
+    proposedLandUse?: ProposedLandUse
+    height?: number
+    constructionTime?: string
+    applicationNumber?: string
+    housing?: Housing
+    showHousing?: boolean
+    showHealthcare?: boolean
+    healthcareDemand?: number
+    showOpenSpace?: boolean
+    openSpaceArea?: number
+    showJobs?: boolean
+    jobs?: Jobs
+    showCarbon?: boolean
+    carbonEmissions?: number
+    showAccess?: boolean
+
+
+
+}
+
+export type DataTypeArray = {
+    data: Data[]
   }
 
 export type PaginationType = {
+    dataId: {_id: string}[]
     data: Data[]
-    paginationData?: Data[]
 }
