dc1fad4eec898c64c4994085cc30b9a8f5b0c5c7
gabimartini <gabimartinii@hotmail.com>
2024-01-11 14:16:17 +0000

    create gallery component

---
+++ src/components/gallery/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import { Button } from "../button"
+const Gallery = ({images, loadImage, setIsModalOpen, setLoadImage, setImageSelected, imageSelected}:any) => {
+    return(
+        <>
+                <div className="carousel-wrap">
+                    <Image src={images[0].original} alt="" width={323} height={240} onClick={() => {setIsModalOpen(true), setImageSelected(imageSelected)}}/>
+                    <div className="carousel-image-list">
+                    {images?.slice(0, loadImage).map((el:any, index :any)=> {
+                        return (
+                            <div key={index} onClick={() => {setIsModalOpen(true), setImageSelected(el.original)}}>
+                            <Image src={el.thumbnail} alt="" width={100} height={80}/>
+                            </div>
+
+                        )
+                    })}
+</div>
+{
+    images.length > loadImage && <Button onClick={() => setLoadImage(images.length)} content={`Load ${images.length - loadImage} more images`} className="govuk-button--secondary gallery-image-button"/>
+}
+
+        </div>
+        </>
+    )
+}
+
+export default Gallery
