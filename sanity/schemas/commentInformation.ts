80ece4fe7febf68675773b61ba5f0eb024a1446d
gabimartini <gabimartinii@hotmail.com>
2024-01-30 13:12:31 +0000

    add space on schema title

--- sanity/schemas/commentInformation.ts
+++ sanity/schemas/commentInformation.ts
@@ -1,7 +1,7 @@
 import { defineField, defineType } from "sanity";
 
 export default defineType({
-    title: "CommentInformation",
+    title: "Comment Information",
     name: "comment-information",
     type: "document",
     fields: [
