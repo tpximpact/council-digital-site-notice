2f0036111076df4c2637319fb8dc2d35a5549d27
gabimartini <gabimartinii@hotmail.com>
2024-02-01 15:51:42 +0000

    add validation feedback to personal-details

--- src/pages/[id]/feedback/questions/personal-details.tsx
+++ src/pages/[id]/feedback/questions/personal-details.tsx
@@ -12,6 +12,7 @@ const PersonalDetails = () => {
     const { onChangeQuestion, setQuestion, selectedCheckbox, personalDetailsForm, setPersonalDetailsForm } = useContext(ContextApplication);
     const [isError, setIsError] = useState<boolean>(false)
     const [isConsentError, setIsConsentError] = useState<boolean>(false)
+    const [checkErrorArr, setCheckErrorArr] = useState<string[]>([])
     
     const backComponent = selectedCheckbox && selectedCheckbox[selectedCheckbox?.length - 1]
 
@@ -38,6 +39,8 @@ const PersonalDetails = () => {
     const onChangeDetails = (value:any, key:any) => {
         setPersonalDetailsForm({...personalDetailsForm, [key]: value})
         localStorage.setItem(key, value)
+        setCheckErrorArr([...checkErrorArr, key])
+        localStorage.setItem(key, value)
         }
 
     const nextPage = () => {
@@ -55,16 +58,48 @@ const PersonalDetails = () => {
             if(phoneValidation && emailValidation) {
                 onChangeQuestion()
             } else {
+                !phoneRegex.test(phoneForm) && setCheckErrorArr([...checkErrorArr, 'phone'])
+                !emailForm.includes('@') && setCheckErrorArr([...checkErrorArr, 'email'])
                 setIsError(true)
             }
                 setIsError(false)
         } else {
+
             setIsError(true)
+
         }
 
             consentForm === false ? setIsConsentError(true) : setIsConsentError(false)
         }
+// validation error message
+const name = personalDetailsForm['name'] === "" ? 'name' : ""
+const address = personalDetailsForm['address'] === "" ? 'address' : ""
+const postcode = personalDetailsForm['postcode'] === "" ? 'postcode' : ""
+const postcodeValid = postCodeRegex.test(postcode)
 
+const messageError = () => {
+    let messageArr = [name, address, postcode].filter((el) => el !== "")
+    const messageArrSize : any= {
+        1:  messageArr[0],
+        2: `${messageArr[0]} and ${messageArr[1]}`,
+        3: `${messageArr[0]}, ${messageArr[1]} and ${messageArr[2]}`
+    }
+    return `${messageArrSize[messageArr.length]} can not be empty` 
+}
+
+function optionalValidation() {
+    const phoneValidation = personalDetailsForm['phone'] !== "" ? phoneRegex.test(personalDetailsForm['phone']) == false ? 'telephone number' : '' : ''
+    const emailValidation = personalDetailsForm['email'] !== "" ? personalDetailsForm['email'].includes('@') == false ? 'email' : '' : ''
+    const validationMessageArr = [phoneValidation, emailValidation].filter((el) => el !== "")
+  
+    const messageValidationArrSize : any = {
+        1: `${validationMessageArr[0]} is invalid`,
+        2: `${validationMessageArr[0]} and ${validationMessageArr[1]} are invalid`
+    } 
+    return messageValidationArrSize[validationMessageArr.length]
+}
+
+messageError()
     return(
         <section className="wrap-personal-details">
         <BackLink content='Back'onClick={() => setQuestion(backComponent)}/>
@@ -80,12 +115,16 @@ const PersonalDetails = () => {
         id='consent' 
         onChange={(e) => {setPersonalDetailsForm({...personalDetailsForm, consent: e.target.checked}), localStorage.setItem('consent', e.target.checked)}} checked={personalDetailsForm?.consent}/>
         </div>
+
         {
-            isError && <Validation message='Name, address and postcode can not be empty'/>   
+            (isError || isConsentError) && <Validation 
+            message={messageError()} 
+            invalidPostCode={(personalDetailsForm['postcode'] !== "" && !postcodeValid) && 'postcode is invalid'} 
+            optionalValidation={optionalValidation()}
+            consentError={isConsentError && `you need to check the consent box`}
+            />   
         }
-        {
-            isConsentError && <Validation message='You need to check the consent box'/>   
-        }
+
         <Details summary="How we handle your data" description={descriptionDetail['consent']} />
         <Button content="Next" onClick={() => nextPage()}/>
         </section>
