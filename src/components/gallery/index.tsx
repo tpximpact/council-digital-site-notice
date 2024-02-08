82475fe078507dcfd9ca230fc8172a8c3dd723b8
gabimartini <gabimartinii@hotmail.com>
2024-02-08 12:01:03 +0000

    gallery main image same image_head

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image"
 import { Button } from "../button"
 import { urlFor } from "../../../util/client"
-const Gallery = ({images, loadImage, setIsModalOpen, setLoadImage, setImageSelected, imageSelected}:any) => {
+const Gallery = ({images, loadImage, setIsModalOpen, setLoadImage, setImageSelected, imageSelected, image_head}:any) => {
     return(
         <>
                 <div className="carousel-wrap">
-                    <Image src={urlFor(images?.[0])?.url()} alt="" width={323} height={240} onClick={() => {setIsModalOpen(true), setImageSelected(imageSelected)}}/>
-                    <div className="carousel-image-list">
-                    {images?.slice(0, loadImage).map((el:any, index :any)=> {
+                    <Image src={urlFor(image_head ? image_head : images?.[0])?.url()} alt="" width={323} height={240} onClick={() => {setIsModalOpen(true), setImageSelected(imageSelected)}}/>
+                    <div className="carousel-image-list"> 
+                    { images && images?.slice(0, loadImage).map((el:any, index :any)=> {
                         return (
                             <div key={index} onClick={() => {setIsModalOpen(true), setImageSelected(el)}}>
                             <Image src={urlFor(el).url()} alt="" width={100} height={80}/>
