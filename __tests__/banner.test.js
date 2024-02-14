b8b0107c238d285afa61854efe716f947f9a5001
gabimartini <gabimartinii@hotmail.com>
2024-02-14 08:51:44 +0000

    fix banner test

--- __tests__/banner.test.js
+++ __tests__/banner.test.js
@@ -2,7 +2,14 @@ import Banner from "../src/components/banner";
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
-test("it should render correctly", () => {
-    render(<Banner />)
-    expect(screen.getByRole('link', {name: 'feedback'}))
+describe("Test Banner", () => {
+    beforeEach(() => {
+        localStorage.setItem('globalInfo', JSON.stringify({feedbackUrl: '/'}))
+    })
+
+    it('it should render correctly', async() => {
+        render(<Banner />)
+        expect(screen.getByRole('link', {name: 'feedback'}))
+    })
+
 })
