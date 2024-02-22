5b19364b7c087d691ceb2e0928b9c475f480a1bc
duncan <duncanuk@gmail.com>
2024-02-22 12:17:20 +0000

    adds site title

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -7,7 +7,7 @@ import Process from "./components/process";
 import { DataDetails } from "../../../util/type";
 import { getActiveApplications, getApplicationById } from "../../../util/client";
 import moment from 'moment'
-
+import Head from 'next/head';
 
 export async function getServerSideProps(context: any) {
   const {id} = context.params;
@@ -65,6 +65,9 @@ const breadcrumbs_array = [{name: "Planning applications", href: "/"}, {name: da
 const {showAccess, showCarbon, showHealthcare, showHousing, showJobs, showOpenSpace} = data || {}
     return (
         <>
+         <Head>
+          <title>Digital site notice</title>
+        </Head>
         <Breadcrumbs breadcrumbs_info={breadcrumbs_array}/>
         <About data={data}/>
         {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import { PaginationType, Data } from '../../util/type';
 import { getLocationFromPostcode } from "../../util/geolocation";
 import { ContextApplication } from '@/context';
 import Link from 'next/link';
+import Head from 'next/head';
 
 export const itemsPerPage = 6;
 
@@ -67,6 +68,9 @@ const Home = ({ dataId, data, globalContent }: PaginationType) => {
 
   return (
     <div className="wrap-home">
+      <Head>
+        <title>Digital site notice</title>
+      </Head>
       <h1 className="govuk-heading-xl" role="heading" style={{display: 'inline-block'}}>
         Find planning applications near you
       </h1>
