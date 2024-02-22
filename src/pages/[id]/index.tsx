bfc6c0b2c7c9aa1daa71b628939f5e6daec487a2
duncan <duncanuk@gmail.com>
2024-02-22 16:10:08 +0000

    test build

--- src/pages/[id]/index.tsx
+++ src/pages/[id]/index.tsx
@@ -18,6 +18,7 @@ export async function getServerSideProps(context: any) {
   };
 }
 
+//enable when go live happens
 // export async function getStaticPaths() {
 //   const data = await getActiveApplications();
   
