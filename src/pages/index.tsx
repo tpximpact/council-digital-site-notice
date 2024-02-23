8d11535e44e83331d7794e4507b5d17c8be3e482
duncan <duncanuk@gmail.com>
2024-02-23 08:16:06 +0000

    moves to static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ import Link from 'next/link';
 
 export const itemsPerPage = 6;
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const dataId = await getActiveApplications();
   const data = await getActiveApplicationsPagination({ itemsPerPage });
   const globalContent = await getGlobalContent()
