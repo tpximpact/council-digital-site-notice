3f3a6f1eef707b137640d26dd9b43ff7ef2c5f06
duncan <duncanuk@gmail.com>
2024-03-04 23:34:26 +0000

    updates classes with new functionality

--- util/dataService.ts
+++ util/dataService.ts
@@ -1,4 +1,5 @@
-// this class will take sanityClient and openDataClient as parameters and will be used to fetch data from the API's and combin the outputs
+/* this class will take sanityClient and openDataClient as parameters 
+   and will be used to fetch data from the API's and combin the outputs */
 export class DataClient {
   private sanityClient: any;
   private openDataClient: any;
@@ -8,10 +9,50 @@ export class DataClient {
     this.openDataClient = openDataClient;
   }
 
-  async getActiveApplications(): Promise<any[]> {
-    const cmsData = await this.sanityClient.getActiveApplications();
-    const openDataPosts =
-      await this.openDataClient.getOpenDataApplicationsPagination(cmsData);
-    return openDataPosts;
+  async getSiteNotices(): Promise<any[]> {
+    const resultData = await this.sanityClient.getActiveApplications();
+    let openDataPosts = [];
+    if (process.env.NEXT_PUBLIC_DATA_PROVIDER == "OpenData") {
+      openDataPosts =
+        await this.openDataClient.getOpenDataApplications(resultData);
+      console.log("openDataPosts", openDataPosts);
+    }
+    return this.getActiveApplicationsCombined(resultData, openDataPosts);
+  }
+
+  private async getActiveApplicationsCombined(
+    resultData: any[],
+    openDataPosts: any[],
+  ) {
+    const developments = resultData.map((siteNotice: any) => {
+      if (
+        !openDataPosts ||
+        !Array.isArray(openDataPosts) ||
+        openDataPosts.length === 0
+      ) {
+        return siteNotice;
+      }
+      const matchedData = openDataPosts.find(
+        (development: any) =>
+          development.application_number === siteNotice.applicationNumber,
+      );
+      console.log("matchedData", matchedData);
+      if (matchedData) {
+        return {
+          ...siteNotice,
+          applicationType: matchedData.application_type,
+          description: matchedData.development_description,
+          address: matchedData.development_address,
+          name: siteNotice.name
+            ? siteNotice.name
+            : matchedData.development_address,
+          location: { lng: matchedData.longitude, lat: matchedData.latitude },
+        };
+      }
+
+      return siteNotice;
+    });
+
+    return developments;
   }
 }
--- util/openDataClient.ts
+++ util/openDataClient.ts
@@ -1,22 +1,25 @@
 //this class is used to make requests to the open data api
 export class OpenDataClient {
-  async getOpenDataApplicationsPagination(cmsData: any[]) {
+  async getOpenDataApplications(cmsData: any[]) {
+    if (!cmsData || cmsData.length === 0) {
+      return [];
+    }
     const limit = 50;
     const arrayToSoqlString = (arr: any[]) =>
       "'" + arr.toString().replace(/,/g, "','") + "'";
+    const ids = cmsData.map(
+      (development: any) => development.applicationNumber,
+    );
+    const orderQuery = `registered_date DESC, last_uploaded DESC`;
+    const whereQuery = `application_number in(${arrayToSoqlString(ids)})`;
     // Fetch the matching data from Camden's API
     const soql = `SELECT * FROM ${process.env.NEXT_PUBLIC_OPEN_DATA_TABLE_NAME} WHERE application_id IN (${arrayToSoqlString(
       cmsData.map((x) => x.application_id),
     )}) LIMIT ${limit}`;
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_OPEN_DATA_URL}/query?soql=${soql}`,
-      {
-        headers: {
-          "X-App-Token": process.env.NEXT_PUBLIC_OPEN_DATA_APP_TOKEN,
-        },
-      },
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}.json?$limit=${limit}&$where=${whereQuery}&$order=${orderQuery}`,
     );
-    const data = await response.json();
+    const data = await res.json();
     return data;
   }
 }
