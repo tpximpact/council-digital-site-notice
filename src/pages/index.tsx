0d3d438fdbdc3b0d5863cc8ebfb718e88114b754
duncan <duncanuk@gmail.com>
2024-03-05 16:07:33 +0000

    adds location sorting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,15 @@ import { OpenDataClient } from "../../util/openDataClient";
 import ReactPaginate from "react-paginate";
 import { NextIcon } from "../../public/assets/icons";
 import { PreviewIcon } from "../../public/assets/icons";
+import Input from "@/components/input";
+import { Button } from "@/components/button";
+import { ArrowIcon } from "../../public/assets/icons";
+import Link from "next/link";
+import { getLocationFromPostcode } from "../../util/geolocation";
+import { distanceInMiles } from "../../util/geolocation";
+import { getGlobalContent } from "../../util/client";
+
+const globalContent = await getGlobalContent();
 
 export const itemsPerPage = 2;
 const dataClient = new DataClient(new SanityClient(), new OpenDataClient());
@@ -49,9 +58,80 @@ const Home = ({ data, globalContent }: PaginationType) => {
     setItemOffset(newOffset);
   };
 
+  const onSearchPostCode = async () => {
+    let location;
+
+    if (postcode != null) {
+      setLocationNotFound(false);
+      location = await getLocationFromPostcode(postcode);
+
+      if (location == null) {
+        setLocationNotFound(true);
+      }
+    }
+    setLocation(location);
+    console.log(location);
+    console.log(data);
+
+    if (location) {
+      const sortedData = data.sort((a, b) => {
+        const distanceA = distanceInMiles(
+          location.latitude,
+          location.longitude,
+          a.latitude,
+          a.longitude,
+        );
+        const distanceB = distanceInMiles(
+          location.latitude,
+          location.longitude,
+          b.latitude,
+          b.longitude,
+        );
+        return distanceA - distanceB;
+      });
+      setDisplayData(sortedData);
+    }
+  };
+
   return (
     <div className="wrap-home">
-      <h1>Planning Applications</h1>
+      <h1
+        className="govuk-heading-xl"
+        role="heading"
+        style={{ display: "inline-block" }}
+      >
+        Find planning applications near you
+      </h1>
+      <p className="govuk-body-m">
+        Find, review and leave your comments on planning applications in{" "}
+        {globalContent?.councilName}
+      </p>
+      <section className="search-grid">
+        <Input
+          label="Enter a postcode to find planning applications nearby"
+          type="text"
+          value={postcode}
+          onChange={(e) => setPostcode(e)}
+          isError={locationNotFound}
+          messageError="Please enter a valid postcode"
+        />
+        <Button
+          className="grid-button-search"
+          content="Search"
+          icon={<ArrowIcon />}
+          onClick={() => onSearchPostCode()}
+        />
+        {globalContent?.signUpUrl && (
+          <Link
+            className="govuk-button grid-button-signup govuk-button--secondary"
+            target="_blank"
+            style={{ textDecoration: "none" }}
+            href={`${globalContent?.signUpUrl}`}
+          >
+            Sign up for alerts on applications near you
+          </Link>
+        )}
+      </section>
       {displayData && (
         <PlanningApplications data={currentItems} searchLocation={location} />
       )}
