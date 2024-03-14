766bf6a149ffed9158074ebdc39c6561bcd12f58
Madeleine Williams <maddie.williams@tpximpact.com>
2024-03-14 14:54:23 +0000

    Fix breaking tests

--- __tests__/components/feelings.test.js
+++ __tests__/components/feelings.test.js
@@ -2,9 +2,24 @@ import Feeling from "../../src/components/feelings";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
+beforeEach(() => {
+  Storage.prototype.getItem = jest.fn(() =>
+    JSON.stringify({ id: "123", value: "" }),
+  );
+  Storage.prototype.setItem = jest.fn();
+});
+
 describe("Feeling component", () => {
+  const mockOnChangeQuestion = jest.fn();
+  const mockSetQuestion = jest.fn();
+
   test("renders the component", () => {
-    render(<Feeling />);
+    render(
+      <Feeling
+        onChangeQuestion={mockOnChangeQuestion}
+        setQuestion={mockSetQuestion}
+      />,
+    );
     const headingElement = screen.getByText(
       /How do you feel about this development?/i,
     );
@@ -12,7 +27,12 @@ describe("Feeling component", () => {
   });
 
   test("displays an error message when Next button is clicked without selecting an option", () => {
-    render(<Feeling />);
+    render(
+      <Feeling
+        onChangeQuestion={mockOnChangeQuestion}
+        setQuestion={mockSetQuestion}
+      />,
+    );
     const nextButton = screen.getByText(/Next/i);
     fireEvent.click(nextButton);
     const errorMessage = screen.getByText(/Please select one/i);
