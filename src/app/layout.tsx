239aa8fc35e2816ffc9c3e20244ef8c6be267fb1
Madeleine Williams <maddie.williams@tpximpact.com>
2024-09-13 11:07:50 +0100

    Fix build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,4 @@
 import "../styles/app.scss";
-import type { Metadata } from "next";
-import { getGlobalContent, urlFor } from "./actions/sanityClient";
-
-export const globalContent = await getGlobalContent();
-export async function generateMetadata(): Promise<Metadata> {
-  const resMetadata = await globalContent?.favicon;
-  return {
-    title: "Digital site notice",
-    icons: {
-      icon: resMetadata ? urlFor(resMetadata)?.url() : "/favicon_default.ico",
-    },
-  };
-}
 
 export default function RootLayout({
   children,
