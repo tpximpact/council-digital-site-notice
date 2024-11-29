34e67c150a94112c91a638e85c9ee89707b4eaa2
Han <apricot13@users.noreply.github.com>
2024-11-29 03:14:21 +0000

    header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,39 +4,54 @@ import { urlFor } from "@/app/actions/sanityClient";
 
 const Header = async ({ globalConfig }: any) => {
   return (
-    <header className="govuk-header" data-module="govuk-header">
-      <div className="govuk-header__container govuk-width-container">
-        <div className="govuk-header__logo">
-          <Link
-            href="/"
-            style={{
-              color: "white",
-              textDecoration: "none",
-              fontWeight: "bold",
-            }}
-          >
-            <span
-              className="govuk-header__logotype"
-              style={{ display: "flex", alignItems: "center", gap: "2px" }}
-            >
-              {globalConfig?.logo !== undefined ? (
-                <Image
-                  width={100}
-                  height={35}
-                  alt=""
-                  src={urlFor(globalConfig?.logo)?.url()}
-                />
-              ) : (
-                ""
-              )}
-              <span className="govuk-header__logotype-text" role="definition">
-                Planning applications
-              </span>
-            </span>
+    <header className="dsn-header">
+      {globalConfig?.logo && (
+        <div className="dsn-header__logo">
+          <Link href="/">
+            <Image
+              width={100}
+              height={35}
+              alt=""
+              src={urlFor(globalConfig?.logo)?.url()}
+            />
           </Link>
         </div>
-      </div>
+      )}
+      <div className="dsn-header__title">Planning applications</div>
     </header>
+    // <header className="govuk-header" data-module="govuk-header">
+    //   <div className="govuk-header__container govuk-width-container">
+    //     <div className="govuk-header__logo">
+    //       <Link
+    //         href="/"
+    //         style={{
+    //           color: "white",
+    //           textDecoration: "none",
+    //           fontWeight: "bold",
+    //         }}
+    //       >
+    //         <span
+    //           className="govuk-header__logotype"
+    //           style={{ display: "flex", alignItems: "center", gap: "2px" }}
+    //         >
+    //           {globalConfig?.logo !== undefined ? (
+    //             <Image
+    //               width={100}
+    //               height={35}
+    //               alt=""
+    //               src={urlFor(globalConfig?.logo)?.url()}
+    //             />
+    //           ) : (
+    //             ""
+    //           )}
+    //           <span className="govuk-header__logotype-text" role="definition">
+    //             Planning applications
+    //           </span>
+    //         </span>
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </header>
   );
 };
 
