882755162882cb3433e2809d643e47887c41ef5a
Han <apricot13@users.noreply.github.com>
2024-12-09 18:03:02 +0000

    buttonStart component

---
+++ src/components/buttonStart/ButtonStart.stories.ts
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import ButtonStart from ".";
+
+const meta = {
+  title: "DSN Components/ButtonStart",
+  component: ButtonStart,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: "fullscreen",
+    // https://storybook.js.org/blog/integrate-nextjs-and-storybook-automatically#nextnavigation
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  args: {},
+} satisfies Meta<typeof ButtonStart>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
---
+++ src/components/buttonStart/index.tsx
@@ -0,0 +1,31 @@
+const ButtonStart = ({
+  content,
+  onClick,
+}: {
+  content?: string;
+  onClick?: () => void;
+}) => {
+  return (
+    <button
+      draggable="false"
+      className="govuk-button govuk-button--start"
+      // data-module="govuk-button"
+      onClick={onClick}
+    >
+      {content ?? "Start now"}
+      <svg
+        className="govuk-button__start-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="17.5"
+        height="19"
+        viewBox="0 0 33 40"
+        aria-hidden="true"
+        focusable="false"
+      >
+        <path fill="currentColor" d="M0 0h13l20 20-20 20H0l20-20z" />
+      </svg>
+    </button>
+  );
+};
+
+export default ButtonStart;
