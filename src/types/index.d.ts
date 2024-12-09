3acdaa70a1e0f02c17d3db3885ac3eaa65a2833c
Han <apricot13@users.noreply.github.com>
2024-12-09 17:02:13 +0000

    import DPR govukinitall component

---
+++ src/types/index.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Shared types and interfaces
+ */
+// export * from "./types";
+
+/**
+ * API responses
+ */
+// export * from "./schemas";
+
+/**
+ * Definitions of our various data models
+ */
+// export * from "./definitions";
