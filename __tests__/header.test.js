9b70d8a90d4f482c08ccdfb05a5d0bf68d811a97
Han <apricot13@users.noreply.github.com>
2025-01-16 20:13:15 +0000

    DSNPI-987 fix header test for accessibility

--- __tests__/header.test.js
+++ __tests__/header.test.js
@@ -5,16 +5,30 @@ import Header from "../src/components/header";
 
 expect.extend(toHaveNoViolations);
 
+jest.mock("../src/app/actions/sanityClient", () => ({
+  urlFor: jest.fn(),
+}));
+
 describe("Header", () => {
+  const value = {
+    globalConfig: {
+      logo: {
+        _type: "image",
+        asset: {
+          _ref: "image-5cde9e98a2fa6341f5957fff4f9a98f00f52ab1a-101x36-png",
+          _type: "reference",
+        },
+      },
+    },
+  };
+
   it("should render correctly", async () => {
-    const value = { globalConfig: null };
-    render(await Header(value));
+    render(<Header globalConfig={value} />);
     expect(screen.getByText("Planning applications")).toBeInTheDocument();
   });
 
   it("should have no accessibility violations", async () => {
-    const value = { globalConfig: null };
-    const { container } = render(await Header(value));
+    const { container } = render(<Header globalConfig={value} />);
     const results = await axe(container);
     expect(results).toHaveNoViolations();
   });
