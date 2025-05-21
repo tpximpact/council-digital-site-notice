56cb9e315915459179e97aec09165ec17e59b1e3
Maddie Williams <107464669+madz-lw@users.noreply.github.com>
2025-05-21 08:48:32 +0100

    Update sanityClient.ts

--- src/actions/sanityClient.ts
+++ src/actions/sanityClient.ts
@@ -75,8 +75,8 @@ export async function getActiveApplications(
  * @returns
  */
 export async function getApplicationById(id: string) {
-  const query = `*[_type == "planning-application" && isActive == true && $_id in [_id, planningId] ${requiredFields}][0]`
-  const post = await client.fetch(query, { _id: id })
+  const query = `*[_type == "planning-application" && isActive == true && $_id in [_id, planningId] ${requiredFields}][0]`;
+  const post = await client.fetch(query, { _id: id });
   return post;
 }
 
